5e71f42fc473e0ddadf60b20f9e87bb2d64847a4
IndexOfNull <21084775+IndexOfNull@users.noreply.github.com>
2023-08-19 21:09:19 -0500

    Move fonts into separate folder

--- modules/clock.py
+++ modules/clock.py
@@ -15,7 +15,7 @@ class ClockPlugin(PluginBase):
     async def draw(self):
         dt = datetime.now()
         text = f"{dt.hour:02d}:{dt.minute:02d}" + (f":{dt.second:02d}" if self.show_seconds else "")
-        font = ImageFont.load("assets/unscii-8-alt.pil")
+        font = ImageFont.load("assets/fonts/unscii-8-alt.pil")
         text_dimensions = font.getbbox(text)[2:] # First two items of bounding box are just zeroes
 
         multiline = text_dimensions[0] > self._width
